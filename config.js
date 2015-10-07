fda719e30a8d5d3e04bc77f9cc2cb8bf67dd1f1a
V.Jindal <vj-ug@users.noreply.github.com>
2015-10-07 09:54:57 -0500

    Create config.js

---
+++ config.js
@@ -0,0 +1,15 @@
+var APIKEY = 'DAKe20a56c25eb74349beab5a0bfff9fad6';
+var PASSWORD = '123456a@';
+
+var TEACHER_CALL_USER = 'user1';
+var STUDENT_CALL_USER = 'user2';
+
+var TEACHER_COBROWSE_USER = 'user3';
+var STUDENT_COBROWSE_USER = 'user4';
+
+var SESSION = '{"session_id":"52007725c35f41068cac4fc8db4f2348","session_type":"support","session_status":"inactive","creation_timestamp":1423365412828,"expiry_timestamp":1423387012827,"admin_full_user_id":"user3@mahindra15.com","domain_name":"mahindra15.com"}';
+var SESSION_ID = '52007725c35f41068cac4fc8db4f2348';
+var DOMAIN_NAME = 'mahindra15.com';
+
+var STUDENT_PHONE_NUMBER = '+1' + '6825219682';
+var TEACHER_PHONE_NUMBER = '';
